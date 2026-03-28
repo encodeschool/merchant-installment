3dcbc3a306b0ae46913a5146519b991fd5577a3e
SarvarTulkunov <sarvartulkunov2002@gmail.com>
2026-03-28 09:36:49 +0200

    fix import issues

--- backend/app/models/product.py
+++ backend/app/models/product.py
@@ -1,7 +1,7 @@
 import uuid
 from datetime import datetime, timezone
 from sqlalchemy import Column, String, Boolean, BigInteger, Integer, DateTime, ForeignKey
-from app.core.database import Base
+from ..core.database import Base
 
 
 class Product(Base):
